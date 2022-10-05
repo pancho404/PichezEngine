a2db96835e9d04c57fe9a28051d4d09fbdf4f7d5
pancho404 <franciscosanchez2101@gmail.com>
2022-10-05 09:03:52 -0300

    Changed from TRS model to SRT model.

--- PichezDLL/PichezDLL/entity.cpp
+++ PichezDLL/PichezDLL/entity.cpp
@@ -51,7 +51,8 @@ DllExport glm::vec4 Entity::getScale()
 
 DllExport void Entity::updateModelMatrix()
 {
-	modelMatrix = scaleMat * rotationMat * translateMat;
+	//modelMatrix = scaleMat * rotationMat * translateMat;
+	modelMatrix = translateMat * rotationMat * scaleMat; //SRT
 }
 
 DllExport glm::mat4 Entity::getModelMatrix()
